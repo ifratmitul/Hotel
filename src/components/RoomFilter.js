e54e41164a13b49ae68eba7c487c553dfc7b9a8d
Ifrat Mitul <ifrat.mitul@hotmail.com>
2020-03-30 18:54:14 +0600

    added filter for roomtype and capacity

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import {useContext} from 'react'
+import {RoomContext} from '../Contex'
+import Title from '../components/Title'
+export default function RoomFilter({rooms}) {
 
-export default function RoomFilter() {
+    const context =  useContext(RoomContext);
+    // console.log(context);
+    const {
+        handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast,pets 
+    } = context
+
+    const getUnique = (items, value) =>{
+        return [... new Set(items.map (item => item[value]))]
+    }
+    let types = getUnique(rooms, 'type');
+    //get all
+    types = ['all', ...types]
+    types = types.map((item, index) =>{
+    return <option value = {item} key =  {index}>{item}</option>
+    })
+
+    let guests = getUnique(rooms, 'capacity');
+    guests = [1, ...guests]
+    guests = guests.map((item, index) => {
+        return <option value = {item} key = {index}> {item}</option>
+    })
+    
     return (
-        <div>
-            Hello from room filter
-        </div>
+        <section className = "filter-container">
+            <Title title = "search room"/>
+            <form className = "filter-form"> 
+             {/* {slect type} */}
+             <div className = "form-group">
+                 <label htmlFor = "type">room type</label>
+                 <select name = "type" id = "type" value = {type} className = "form-control" onChange = {handleChange}>
+                   {types}
+                 </select>
+
+                 <select name = "type" id = "type" value = {type} className = "form-control" onChange = {handleChange}>
+                   {guests}
+                 </select>
+
+
+
+
+             </div>
+             
+             
+             
+             {/* {end of select type} */}
+            </form>
+        </section>
     )
 }
