872da5930eae2cc47c88e3a0c9689e2fede3cab2
Ifrat Mitul <ifrat.mitul@hotmail.com>
2020-03-30 11:05:11 +0600

    RoomList Complete, filtering underway

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Room from './Room'
 
-export default function RoomList() {
+export default function RoomList( {rooms}) {
+    
+    if(rooms.length === 0 ){
+        return (
+
+            <div className = "empty-search">
+                <h3>No Rooms mateched your search parameters</h3>
+            </div>
+        )
+    }
+    
     return (
-        <div>
-            Room list
-        </div>
+        <section className = "roomslist">
+            <div className = "roomslist-center">
+                {rooms.map(item =>{
+                    return <Room key = {item.id} room = {item}/>
+                })}
+            </div>
+        </section>
     )
 }
